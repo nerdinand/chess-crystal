9324ac64f76abe0b90f69de93957d0a3afbe6306
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2021-04-12 23:30:18 +0200

    Fix comments

--- src/move_checker/queen_move_checker.cr
+++ src/move_checker/queen_move_checker.cr
@@ -1,7 +1,7 @@
 require "./move_checker"
 
 module Chess
-  # 3.2 The bishop may move to any square along a diagonal on which it stands.
+  # 3.4 The queen may move to any square along the file, the rank or a diagonal on which it stands.
   class QueenMoveChecker < MoveChecker
     def possible?(from_file_index : Int, from_rank_index : Int, to_file_index : Int, to_rank_index : Int) : Bool
       (
--- src/move_checker/rook_move_checker.cr
+++ src/move_checker/rook_move_checker.cr
@@ -1,7 +1,7 @@
 require "./move_checker"
 
 module Chess
-  # 3.2 The bishop may move to any square along a diagonal on which it stands.
+  # 3.3 The rook may move to any square along the file or the rank on which it stands.
   class RookMoveChecker < MoveChecker
     def possible?(from_file_index : Int, from_rank_index : Int, to_file_index : Int, to_rank_index : Int) : Bool
       straight_path?(from_file_index, from_rank_index, to_file_index, to_rank_index) &&
